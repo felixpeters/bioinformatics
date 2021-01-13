ba2574419bb5609e8df76d3b9efde1b8cd810c05
Felix Peters <felix@felixpeters.me>
2021-01-13 08:39:52 +0100

    make file paths absolute for tests

--- prot/prot.py
+++ prot/prot.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from Bio.Seq import Seq
+
 def rna_codon_table(filename):
     with open(filename) as f:
         lines = f.readlines()
@@ -9,8 +12,12 @@ def rna_codon_table(filename):
     return rct
 
 def translate_rna(rna):
-    rct = rna_codon_table("rna_codon_table.txt")
+    rct = rna_codon_table(Path(__file__).absolute().parent / "rna_codon_table.txt")
     codons = [rna[i:i+3] for i in range(0, len(rna), 3)]
     prots = [rct[c] for c in codons]
     prot_str = ''.join(prots)
     return prot_str.split("Stop", 1)[0]
+
+def translate_rna_bp(rna):
+    seq = Seq(rna)
+    return seq.translate(to_stop=True)
--- prot/test_prot.py
+++ prot/test_prot.py
@@ -1,6 +1,13 @@
-from prot import translate_rna
+from prot import translate_rna, translate_rna_bp
 
 def test_translate_rna():
     rna = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
     protein = "MAMAPRTEINSTRING"
     assert translate_rna(rna) == protein
+
+def test_translate_rna_bp():
+    rna = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+    protein = "MAMAPRTEINSTRING"
+    assert str(translate_rna_bp(rna)) == protein
+
+
